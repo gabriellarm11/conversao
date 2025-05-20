9e06b0241b9d57c233b866a8f7b015bb77bde5c5
GABRIELLA ROBERTA MESSIAS DOS SANTOS <gabriella.rmsantos@senacsp.edu.br>
2025-05-20 11:02:55 -0300

    nova atividades

---
+++ conversao.js
@@ -0,0 +1,20 @@
+function converter() {
+    const dolarInput = document.getElementById('dolar');
+    const resultado = document.getElementById('resultado');
+    const dolar = parseFloat(dolarInput.value);
+    const taxaCambio = 5.15; // Cotação fixa
+  
+    if (dolarInput.value.trim() === '' || isNaN(dolar)) {
+      resultado.style.color = 'red';
+      resultado.textContent = "por favor, insira um valor valido";
+      return;
+    }
+  
+    const real = dolar * taxaCambio;
+    
+    resultado.innerHTML = `
+      US$ ${dolar.toFixed(2)}<br>
+      R$ ${real.toFixed(2).replace('.', ',')}
+    `;
+  }
+  
